d1a5ca04c9a65bf80499b7ea19a52b89ba3b2aab
Ваше Имя <you@example.com>
2024-07-14 15:40:54 +0500

    файл processing.ry в ветке feature/homework_10.1

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,17 @@
-def filter_by_state():
+from typing import Any
 
 
-def sort_by_date():
-    pass
+def filter_by_state(list_of_data: Any, state = "EXECUTED") -> Any:
+    '''Функция возвращающая список словарей, отсортированных по "state"'''
+    right_list = []
+    for dict in list_of_data:
+        if dict["state"] == state:
+            right_list.append(dict)
+    return right_list
+
+
+def sort_by_date(list_of_data: Any, key="date", reverse=True) -> Any:
+    '''Функция возвращающая список словарей отсортированных по дате'''
+    return sorted(list_of_data, key=lambda x: x[key], reverse=reverse)
+
 
