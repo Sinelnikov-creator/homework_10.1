e29dfb5e4f1d0ecc392d6982f1aa0a553dc4e965
Ваше Имя <you@example.com>
2024-07-16 00:28:53 +0500

    файл test_masks.py после коррекции

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -15,5 +15,35 @@ def test_get_mask_card_number_correct(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
 
+def test_get_mask_card_number_invalid_len_card():
+    with pytest.raises(ValueError):
+        get_mask_card_number('1234')
+
+
+def test_get_mask_card_number_invalid_len_card_zero():
+    with pytest.raises(ValueError):
+        get_mask_card_number(' ')
+
+
+@pytest.fixture
+def account_number():
+    return ['73654108430135874305', '73654108556635876894', '73654108887735873625']
+
+
+@pytest.mark.parametrize("account_number, expected", [('73654108430135874305', '**4305'),
+                                                      ('73654108556635876894', '**6894'),
+                                                      ('73654108887735873625', '**3625'),])
+def test_get_mask_account_correct(account_number, expected):
+    assert get_mask_account(account_number) == expected
+
+
+def test_get_mask_account_invalid_len():
+    with pytest.raises(ValueError):
+        get_mask_account('1234')
+
+
+def test_get_mask_account_invalid_len_zero():
+    with pytest.raises(ValueError):
+        get_mask_account(' ')
 
 
