59a8923ccec89e6294e58367a56bc39e10876e0b
Ваше Имя <you@example.com>
2024-07-15 23:44:33 +0500

    файл masks.py после коррекции

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,20 @@
 def get_mask_card_number(card_number: str) -> str:
     """Payment card number"""
+    if len(card_number) < 16 or len(card_number) > 16:
+        raise ValueError("Неверная длина карты")
+
+    if len(card_number) == 0:
+        raise ValueError("Пустая строка")
+
     return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Номер счета"""
+    if len(account_number) < 20 or len(account_number) > 20:
+        raise ValueError("Неверная длина номера счета")
+
+    if len(account_number) == 0:
+        raise ValueError("Пустая строка")
+
     return f"**{account_number[-4:]}"
