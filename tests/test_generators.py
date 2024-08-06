76907aa3546971659808cb1e5b9efa5ab0801081
Павел Синельников <spv-71@yandex.ru>
2024-08-06 11:06:38 +0500

    изменение файла test_generators.py в ветке feature/homework_11.1

---
+++ tests/test_generators.py
@@ -0,0 +1,83 @@
+import pytest
+
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+@pytest.fixture
+def get_bad_data():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }]
+
+
+@pytest.fixture
+def get_good_data() -> list[dict]:
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }]
+
+
+def test_filter_by_currency(get_good_data, get_bad_data):
+    assert list(filter_by_currency(get_good_data, "USD")) == [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации", "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        }
+    ]
+    assert list(filter_by_currency(get_good_data, "RUB")) == [
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "RUB", "code": "RUB"}},
+            "description": "Перевод со счета на счет", "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
+    ]
+    assert list(filter_by_currency(get_bad_data, "USD")) == []
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -11,13 +11,13 @@ def list_of_dicts():
             {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
-#@pytest.mark.parametrize("list_of_dicts, expected",
-#                        [{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
+# @pytest.mark.parametrize("list_of_dicts, expected",
+#                        [[{'id': 41428829, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
 #                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 #                         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#                        {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}],
-#                        [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#                        {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}])
+#                         {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}]],
+#                        [[{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                         {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}]])
 def test_filter_by_state(list_of_dicts):
     assert filter_by_state(list_of_dicts) == [
                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
